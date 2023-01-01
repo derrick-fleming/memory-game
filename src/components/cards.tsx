ce7e0b48ff01392fff05a1077e8adeee0f24a2c1
Derrick Fleming <derrick.fleming7@gmail.com>
2023-01-01 21:17:26 +0000

    Updated card component to create a sub-component for just the card to handle changing flipped-state when user clicks on a card.

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -12,22 +12,43 @@ shapes.forEach(shape => {
 const shuffledCards = _.shuffle(cardShapes);
 
 const Cards = function () {
+
   const cardTypes = shuffledCards.map((card, index) => {
     return (
       <div className='flip-container' key={index}>
-        <div className='flipper'>
-          <button className='front card'>
-            <h1 className="hidden fa-solid fa-question"></h1>
-          </button>
-          <button className='card back' id={card}>
-            <h1 className={`fa-solid fa-${card}`}></h1>
-          </button>
-        </div>
-
+        <Card symbol={card}/>
       </div>
       )
   })
   return cardTypes;
+}
+
+const Card = function(props: {symbol: string}){
+  const [flipClass, setFlipClass] = useState(false);
+
+  function handleFlip() {
+    if (flipClass) {
+      setFlipClass(false)
+    } else {
+      setFlipClass(true)
+    }
+  }
+
+  const flippedClass = flipClass ? 'flipped-card' : '';
+
+  return (
+    <div className={`flipper ${flippedClass}`}>
+      <button onClick={handleFlip} className={`front card`}>
+        <h1 className="hidden fa-solid fa-question"></h1>
+      </button>
+      <button className={`back card`} id={props.symbol}>
+        <h1 className={`fa-solid fa-${props.symbol}`}></h1>
+      </button>
+    </div>
+
+  )
+
+
 }
 
 export default Cards;
